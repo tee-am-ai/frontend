6f147f7ada827bbc01298c3d1bc1e662bf82356e
Fedhira Syaila <pedirasyaila@gmail.com>
2024-07-05 00:29:57 +0700

    buat auth

--- assets/js/component/auth.js
+++ assets/js/component/auth.js
@@ -1,14 +1,14 @@
-// import { getCookie } from "https://jscroot.github.io/cookie/croot.js";
+import { getCookie } from "https://jscroot.github.io/cookie/croot.js";
 
-// function checkTokenAndRedirect() {
-//     const tokens = "Authorization";
-//     const tokenValue = getCookie(tokens);
-//     console.log("Token Value:", tokenValue);
+function checkTokenAndRedirect() {
+    const tokens = "Authorization";
+    const tokenValue = getCookie(tokens);
+    console.log("Token Value:", tokenValue);
 
-//     if (!tokenValue) {
-//         console.log("Token not found, redirecting to login.");
-//         window.location.href = "./auth-login.html";
-//     }
-// }
+    if (!tokenValue) {
+        console.log("Token not found, redirecting to login.");
+        window.location.href = "./auth-login.html";
+    }
+}
 
-// window.onload = checkTokenAndRedirect;
+window.onload = checkTokenAndRedirect;
