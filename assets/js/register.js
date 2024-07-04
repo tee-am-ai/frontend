db0a01e2243217b9ca32b4cbf532f8b3396bfa29
Fedhira Syaila <pedirasyaila@gmail.com>
2024-07-05 00:31:32 +0700

    buat register.js

---
+++ assets/js/register.js
@@ -0,0 +1,48 @@
+import { getValue } from "https://jscroot.github.io/element/croot.js";
+
+function postRegister(target_url, data, responseFunction) {
+
+    const requestOptions = {
+        method: 'POST',
+        body: JSON.stringify(data),
+        redirect: 'follow'
+    };
+
+    fetch(target_url, requestOptions)
+        .then(response => response.text())
+        .then(result => responseFunction(JSON.parse(result)))
+        .catch(error => console.log('error', error));
+}
+
+const Register = () => {
+    const target_url = "https://asia-southeast2-teeamai-427702.cloudfunctions.net/teeamai/signup";
+    
+    const data = {
+        "fullname" : getValue("fullname"),
+        "email": getValue("email"),
+        "password": getValue("password"),
+        "confirmpassword": getValue("confirmpassword"),
+    };
+    
+    postRegister(target_url, data, responseData);
+}
+
+function responseData (result) {
+    if (result.status === true) {
+        Swal.fire({
+            icon: "success",
+            title: "Register Successful",
+            text: result.message,
+        }).then(() => {
+            window.location.href = "../signup.html";
+        });
+    } else {
+        Swal.fire({
+            icon: "error",
+            title: "Register Failed",
+            text: result.message,
+        });
+    }
+}
+
+document.getElementById("button1").addEventListener("click", Register);
