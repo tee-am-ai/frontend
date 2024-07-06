3177cd8d5a6461f033b62128843fb46c2aaaf3bf
Fedhira Syaila <pedirasyaila@gmail.com>
2024-07-06 21:05:29 +0700

    tambah si logout

---
+++ assets/js/logout.js
@@ -0,0 +1,23 @@
+import { deleteCookie } from "https://jscroot.github.io/cookie/croot.js";
+
+const logout = () => {
+    Swal.fire({
+      icon: "question",
+      title: "Konfirmasi",
+      text: "Apakah Anda benar ingin logout?",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Benar",
+      cancelButtonText: "Tidak",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        deleteCookie("Authorization");
+        window.location.href = "https://tee-am-ai.github.io/frontend";
+      }
+    });
+  };
+  
+const btnLogout = document.getElementById("btnLogout");
+
+btnLogout.addEventListener("click", logout);
