1b20d80325d663ba6c8056fd445ee2689a6b9d10
ArykaAnisaP <arykaanisap22@gmail.com>
2024-07-10 12:02:55 +0700

    update login.js

--- assets/js/login.js
+++ assets/js/login.js
@@ -10,18 +10,38 @@ function postLogin(target_url, data, responseFunction) {
 
     fetch(target_url, requestOptions)
         .then(response => response.text())
-        .then(result => responseFunction(JSON.parse(result)))
+        .then(result => {
+            try {
+                responseFunction(JSON.parse(result));
+            } catch (error) {
+                console.error('Error parsing response:', error);
+            }
+        })
         .catch(error => console.log('error', error));
 }
 
 const Login = () => {
+    const username = getValue("username");
+    const password = getValue("password");
+
+    if (!username || !password) {
+        Swal.fire({
+            icon: "error",
+            title: "Input Error",
+            text: "Username and password cannot be empty",
+        });
+        return;
+    }
+
     const target_url = "https://asia-southeast2-teeamai-427702.cloudfunctions.net/teeamai/login";
 
     const data = {
-        "username": getValue("username"),
-        "password": getValue("password")
+        "username": username,
+        "password": password
     };
-    
+
+    console.log('Sending data:', data);  // Debugging log
+
     postLogin(target_url, data, responseData);
 }
 
