6e79336d3e5c5fdc20689c47e264501431ecc3a9
ArykaAnisaP <arykaanisap22@gmail.com>
2024-07-10 11:47:28 +0700

    update html + js

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,33 +1,35 @@
 import { getValue } from "https://jscroot.github.io/element/croot.js";
 
-
 function postLogin(target_url, data, responseFunction) {
-
     const requestOptions = {
         method: 'POST',
         body: JSON.stringify(data),
+        headers: { 'Content-Type': 'application/json' },
         redirect: 'follow'
     };
 
     fetch(target_url, requestOptions)
-        .then(response => response.text())
-        .then(result => responseFunction(JSON.parse(result)))
+        .then(response => response.json())
+        .then(result => responseFunction(result))
         .catch(error => console.log('error', error));
 }
 
 const Login = () => {
     const target_url = "https://asia-southeast2-teeamai-427702.cloudfunctions.net/teeamai/login";
     
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+    
     const data = {
-        "username": getValue("username"),
-        "password": getValue("password"),
+        "username": username,
+        "password": password,
     };
     
     postLogin(target_url, data, responseData);
 }
 
-function responseData (result) {
-    if (result.error === undefined || !result.error) {
+function responseData(result) {
+    if (!result.error) {
         Swal.fire({
             icon: "success",
             title: "Login Successful",
