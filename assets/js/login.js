3e731593fbc00e780146946fad57e14cd6e22967
ArykaAnisaP <arykaanisap22@gmail.com>
2024-07-10 12:08:44 +0700

    update js

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,52 +1,32 @@
 import { getValue } from "https://jscroot.github.io/element/croot.js";
 
 function postLogin(target_url, data, responseFunction) {
+
     const requestOptions = {
         method: 'POST',
         body: JSON.stringify(data),
-        headers: { 'Content-Type': 'application/json' },
         redirect: 'follow'
     };
 
     fetch(target_url, requestOptions)
         .then(response => response.text())
-        .then(result => {
-            try {
-                responseFunction(JSON.parse(result));
-            } catch (error) {
-                console.error('Error parsing response:', error);
-            }
-        })
+        .then(result => responseFunction(JSON.parse(result)))
         .catch(error => console.log('error', error));
 }
 
 const Login = () => {
-    const username = getValue("username");
-    const password = getValue("password");
-
-    if (!username || !password) {
-        Swal.fire({
-            icon: "error",
-            title: "Input Error",
-            text: "Username and password cannot be empty",
-        });
-        return;
-    }
-
     const target_url = "https://asia-southeast2-teeamai-427702.cloudfunctions.net/teeamai/login";
-
+    
     const data = {
-        "username": username,
-        "password": password
+        "username": getValue("username"),
+        "password": getValue("password"),
     };
-
-    console.log('Sending data:', data);  // Debugging log
-
+    
     postLogin(target_url, data, responseData);
 }
 
 function responseData(result) {
-    if (!result.error) {
+    if (result.error === undefined || !result.error) {
         Swal.fire({
             icon: "success",
             title: "Login Successful",
