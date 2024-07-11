2e370307f74abbbf3029d88bac298cdb77ddc573
dirgafebrian <fdirga63@gmail.com>
2024-07-11 23:24:35 +0700

    add back-to-top button positioning

--- assets/js/sidebar.js
+++ assets/js/sidebar.js
@@ -1,12 +1,16 @@
 const sidebar = document.getElementById("sidebar");
 const sidebarCollapse = document.getElementById("sidebarCollapse");
 const inputArea = document.getElementById("input-area");
+const backToTop = document.querySelector(".back-to-top");
 
 sidebarCollapse.addEventListener("click", function () {
   sidebar.classList.toggle("active");
   if (sidebar.classList.contains("active")) {
     inputArea.style.marginLeft = "32px"; 
+    backToTop.style.right = "49%";
   } else {
     inputArea.style.marginLeft = "335px";
+    backToTop.style.right = "41%";
   }
 });
+
