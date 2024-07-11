ad26cdb3bde3645428ad3b4ff31103df39241531
Novi <alnoviantiramadhani91@gmail.com>
2024-07-11 18:33:16 +0700

    membuat function js logout

--- assets/js/logout.js
+++ assets/js/logout.js
@@ -1,23 +1,28 @@
 import { deleteCookie } from "https://jscroot.github.io/cookie/croot.js";
 
 const logout = () => {
-    Swal.fire({
-      icon: "question",
-      title: "Konfirmasi",
-      text: "Apakah Anda benar ingin logout?",
-      showCancelButton: true,
-      confirmButtonColor: "#3085d6",
-      cancelButtonColor: "#d33",
-      confirmButtonText: "Benar",
-      cancelButtonText: "Tidak",
-    }).then((result) => {
-      if (result.isConfirmed) {
-        deleteCookie("Authorization");
-        window.location.href = "./signin.html";
-      }
-    });
-  };
-  
-const btnLogout = document.getElementById("btnLogout");
+  Swal.fire({
+    icon: "question",
+    title: "Konfirmasi",
+    text: "Apakah Anda benar ingin logout?",
+    showCancelButton: true,
+    confirmButtonColor: "#3085d6",
+    cancelButtonColor: "#d33",
+    confirmButtonText: "Benar",
+    cancelButtonText: "Tidak",
+  }).then((result) => {
+    if (result.isConfirmed) {
+      window.location.href = "./signin.html";
+    }
+  });
+};
 
-btnLogout.addEventListener("click", logout);
+document.addEventListener("DOMContentLoaded", () => {
+  const btnLogout = document.getElementById("btnLogout");
+  if (btnLogout) {
+    btnLogout.addEventListener("click", (event) => {
+      event.preventDefault(); // Prevent the default link behavior
+      logout();
+    });
+  }
+});
