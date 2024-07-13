6933db3bc91528236fbf3d7bdbb117239c1cc56d
Fedhira Syaila <pedirasyaila@gmail.com>
2024-07-14 01:03:23 +0700

    update logout

--- assets/js/logout.js
+++ assets/js/logout.js
@@ -1,21 +1,8 @@
 import { deleteCookie } from "https://jscroot.github.io/cookie/croot.js";
 
 const logout = () => {
-  Swal.fire({
-    icon: "question",
-    title: "Konfirmasi",
-    text: "Apakah Anda benar ingin logout?",
-    showCancelButton: true,
-    confirmButtonColor: "#3085d6",
-    cancelButtonColor: "#d33",
-    confirmButtonText: "Benar",
-    cancelButtonText: "Tidak",
-  }).then((result) => {
-    if (result.isConfirmed) {
-      deleteCookie("Authorization"); //Destroy cookies
-      window.location.href = "./signin.html";
-    }
-  });
+  deleteCookie("Authorization"); // Destroy cookies
+  window.location.href = "./signin.html"; // Redirect to signin page
 };
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -27,3 +14,32 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   }
 });
+
+// const logout = () => {
+//   Swal.fire({
+//     icon: "question",
+//     title: "Konfirmasi",
+//     text: "Apakah Anda benar ingin logout?",
+//     showCancelButton: true,
+//     confirmButtonColor: "#3085d6",
+//     cancelButtonColor: "#d33",
+//     confirmButtonText: "Benar",
+//     cancelButtonText: "Tidak",
+//   }).then((result) => {
+//     if (result.isConfirmed) {
+//       deleteCookie("Authorization"); //Destroy cookies
+//       window.location.href = "./signin.html";
+//     }
+//   });
+// };
+
+// document.addEventListener("DOMContentLoaded", () => {
+//   const btnLogout = document.getElementById("btnLogout");
+//   if (btnLogout) {
+//     btnLogout.addEventListener("click", (event) => {
+//       event.preventDefault(); // Prevent the default link behavior
+//       logout();
+//     });
+//   }
+// });
+
