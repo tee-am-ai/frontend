f1ac247bef30ecc344a52c282e0af9dfdccfb1e6
dirgafebrian <fdirga63@gmail.com>
2024-07-16 20:26:33 +0700

    Update sidebar and chat box margins for smaller screens

--- assets/js/sidebar.js
+++ assets/js/sidebar.js
@@ -2,6 +2,7 @@ const sidebar = document.getElementById("sidebar");
 const sidebarCollapse = document.getElementById("sidebarCollapse");
 const inputArea = document.getElementById("input-area");
 const backToTop = document.querySelector(".back-to-top");
+const chatBox = document.getElementById("chat-box");
 
 sidebarCollapse.addEventListener("click", function () {
   const screenWidth = window.innerWidth;
@@ -10,9 +11,11 @@ sidebarCollapse.addEventListener("click", function () {
   if (screenWidth <= 1199) {
     if (active) {
       inputArea.style.marginLeft = "335px";
+      chatBox.style.marginLeft = "335px";
       backToTop.style.right = "49%";
     } else {
       inputArea.style.marginLeft = "32px";
+      chatBox.style.marginLeft = "32px";
       backToTop.style.right = "41%";
     }
   } else {
