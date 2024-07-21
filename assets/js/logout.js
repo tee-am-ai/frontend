aaafa2b73e42a837c3566243f130b2863bf4fde5
Fedhira Syaila <pedirasyaila@gmail.com>
2024-07-21 15:53:39 +0700

    update logout

--- assets/js/logout.js
+++ assets/js/logout.js
@@ -15,31 +15,3 @@ document.addEventListener("DOMContentLoaded", () => {
   }
 });
 
-// const logout = () => {
-//   Swal.fire({
-//     icon: "question",
-//     title: "Konfirmasi",
-//     text: "Apakah Anda benar ingin logout?",
-//     showCancelButton: true,
-//     confirmButtonColor: "#3085d6",
-//     cancelButtonColor: "#d33",
-//     confirmButtonText: "Benar",
-//     cancelButtonText: "Tidak",
-//   }).then((result) => {
-//     if (result.isConfirmed) {
-//       deleteCookie("Authorization"); //Destroy cookies
-//       window.location.href = "./signin.html";
-//     }
-//   });
-// };
-
-// document.addEventListener("DOMContentLoaded", () => {
-//   const btnLogout = document.getElementById("btnLogout");
-//   if (btnLogout) {
-//     btnLogout.addEventListener("click", (event) => {
-//       event.preventDefault(); // Prevent the default link behavior
-//       logout();
-//     });
-//   }
-// });
-
