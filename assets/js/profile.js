16a69784d2e30ce2a5da4ac8ceccbd5f866458a7
Fedhira Syaila <pedirasyaila@gmail.com>
2024-07-22 19:21:28 +0700

    update profile js

--- assets/js/profile.js
+++ assets/js/profile.js
@@ -1,45 +1,43 @@
-function getCookie(name) {
-    let cookieArr = document.cookie.split(";");
-    for (let i = 0; i < cookieArr.length; i++) {
-        let cookiePair = cookieArr[i].split("=");
-        if (name === cookiePair[0].trim()) {
-            return decodeURIComponent(cookiePair[1]);
-        }
-    }
-    return null;
-}
+// function getCookie(name) {
+//     let cookieArr = document.cookie.split(";");
+//     for (let i = 0; i < cookieArr.length; i++) {
+//         let cookiePair = cookieArr[i].split("=");
+//         if (name === cookiePair[0].trim()) {
+//             return decodeURIComponent(cookiePair[1]);
+//         }
+//     }
+//     return null;
+// }
 
-function getUser(target_url, responseFunction) {
-    const myHeaders = new Headers();
-    myHeaders.append("Authorization", getCookie("Authorization"));
+// function getUser(target_url, responseFunction) {
+//     const myHeaders = new Headers();
+//     myHeaders.append("Authorization", getCookie("Authorization"));
 
-    const requestOptions = {
-        method: 'GET',
-        headers: myHeaders,
-        redirect: 'follow'
-    };
+//     const requestOptions = {
+//         method: 'GET',
+//         headers: myHeaders,
+//         redirect: 'follow'
+//     };
 
-    fetch(target_url, requestOptions)
-        .then(response => response.json())
-        .then(result => responseFunction(result))
-        .catch(error => console.log('error', error));
-}
+//     fetch(target_url, requestOptions)
+//         .then(response => response.text())
+//         .then(result => responseFunction(JSON.parse(result)))
+//         .catch(error => console.log('error', error));
+// }
 
-const apiUrl = "https://api-tee-am-ai.up.railway.app/user";
+// const apiUrl = "https://api-tee-am-ai.up.railway.app/user";
 
-function displayUserData(data) {
-    document.getElementById("username-display").textContent = data.data.namalengkap;
-    document.getElementById("email-display").textContent = data.data.email;
-}
+// function displayUserData(data) {
+//     document.getElementById("username-display").textContent = data.data.namalengkap;
+//     document.getElementById("email-display").textContent = data.data.email;
+// }
 
-function responseUserData(result) {
-    if (result.error === undefined || !result.error) {
-        displayUserData(result);
-    } else {
-        console.error("Error fetching user data:", result.message);
-    }
-}
+// function responseUserData(result) {
+//     if (result.error === undefined || !result.error) {
+//         displayUserData(result);
+//     } else {
+//         console.error("Error fetching user data:", result.message);
+//     }
+// }
 
-document.addEventListener("DOMContentLoaded", function() {
-    getUser(apiUrl, responseUserData);
-});
+// getUser(apiUrl, responseUserData);
