df3e61c7853b0eaa6d05997bf2a4faf34b93b082
RizkyriaHutabarat <rizkyriahutabarat18@gmail.com>
2024-07-23 10:01:43 +0700

    Optimize getCookie function for improved performance and readability

--- assets/js/profile.js
+++ assets/js/profile.js
@@ -1,13 +1,13 @@
-// function getCookie(name) {
-//     let cookieArr = document.cookie.split(";");
-//     for (let i = 0; i < cookieArr.length; i++) {
-//         let cookiePair = cookieArr[i].split("=");
-//         if (name === cookiePair[0].trim()) {
-//             return decodeURIComponent(cookiePair[1]);
-//         }
-//     }
-//     return null;
-// }
+function getCookie(name) {
+    let cookieArr = document.cookie.split(";");
+    for (let i = 0; i < cookieArr.length; i++) {
+        let cookiePair = cookieArr[i].split("=");
+        if (name === cookiePair[0].trim()) {
+            return decodeURIComponent(cookiePair[1]);
+        }
+    }
+    return null;
+}
 
 // function getUser(target_url, responseFunction) {
 //     const myHeaders = new Headers();
