b564a4d0c8794a39ec21bcf7ee28e3c1ad4ac519
Novi <alnoviantiramadhani91@gmail.com>
2024-07-23 22:19:24 +0700

    Update requestoptions

--- assets/js/checklsLogin.js
+++ assets/js/checklsLogin.js
@@ -1,5 +1,9 @@
 import { getValue } from "https://jscroot.github.io/element/croot.js";
 
 function postLogin(target_url, data, responseFunction) {
-  const requestOptions = {};
+  const requestOptions = {
+    method: "POST",
+    body: JSON.stringify(data),
+    redirect: "follow",
+  };
 }
