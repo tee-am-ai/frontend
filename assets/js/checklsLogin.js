288e3ccfb7c8f4441c8060b141d76aebf1aab672
Novi <alnoviantiramadhani91@gmail.com>
2024-07-23 22:29:56 +0700

    Refactor postLogin function to include request options

--- assets/js/checklsLogin.js
+++ assets/js/checklsLogin.js
@@ -1,3 +1,9 @@
 import { getValue } from "https://jscroot.github.io/element/croot.js";
 
-function postLogin(target_url, data, responseFunction) {}
+function postLogin(target_url, data, responseFunction) {
+  const requestOptions = {
+    method: "POST",
+    body: JSON.stringify(data),
+    redirect: "follow",
+  };
+}
