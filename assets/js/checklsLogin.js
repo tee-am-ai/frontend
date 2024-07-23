d442a29e4359825ccd5692ea4d77e43be4f5a267
Novi <alnoviantiramadhani91@gmail.com>
2024-07-23 21:26:30 +0700

    Add postLogin function for handling login requests

--- assets/js/checklsLogin.js
+++ assets/js/checklsLogin.js
@@ -1,1 +1,8 @@
 import { getValue } from "https://jscroot.github.io/element/croot.js";
+
+function postLogin(target_url, data, responseFunction) {
+  const requestOptions = {
+    method: "POST",
+    body: JSON.stringify(data),
+  };
+
