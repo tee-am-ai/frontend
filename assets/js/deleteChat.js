1cf3e7153dce0094e6732e5daa9c0f50d82b7bf7
Fatwaff <fax.mp4@gmail.com>
2024-07-28 01:38:22 +0700

    Delete deleteChat.js file

--- assets/js/deleteChat.js
+++
@@ -1,7 +0,0 @@
-import { deleteChat } from "./chat.js";
-console.log("tes");
-console.log(document.getElementById("delete-chat"));
-document.getElementById("delete-chat").addEventListener("click", function () {
-    console.log("delete chat");
-    deleteChat();
-});
